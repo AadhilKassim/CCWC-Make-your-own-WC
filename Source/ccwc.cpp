c067ff023ea49acc122f668dd2995ace78acbdc6
Aadhil Kassim <aadhikassim@gmail.com>
2024-12-27 11:46:05 +0530

    Fixed spelling in the error output at line 40
    From Unkown to Unknown

--- Source/ccwc.cpp
+++ Source/ccwc.cpp
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
             }
         }else
         {
-            std::cerr << "Unkown option";
+            std::cerr << "Unknown option";
 
         }
     }else
